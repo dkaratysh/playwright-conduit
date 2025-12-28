c43d321497628c8ea9ca54c6df258d351549f4dd
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2025-12-28 15:26:41 +0100

    Fixed CI settings for mocked tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,42 @@
 import { defineConfig, devices } from '@playwright/test';
 import 'dotenv/config';
 
+const baseURL =
+  process.env.BASE_URL ?? 'https://conduit-realworld-example-app.fly.dev';
 export default defineConfig({
+  use: {
+  baseURL,
+},
   testDir: './tests',
-
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
   projects: [
     {
       name: 'setup',
       testMatch: /.*auth\.setup\.spec\.ts/,
+      timeout: 120_000,
+      use:{
+        baseURL,
+      }
     },
     {
       name: 'chromium',
+      testDir: './tests/ui',
       dependencies: ['setup'],
       use: {
         ...devices['Desktop Chrome'],
-        baseURL: process.env.BASE_URL,
         storageState: '.auth/user.json',
       },
     },
-  ],
+    {
+    name: 'ui-mock',
+    testDir: './tests/ui-mocked',
+    use: {
+      ...devices['Desktop Chrome'],
+    },
+  },
+],
 
   timeout: 60_000,
 });
