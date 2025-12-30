d1d7b948d645b8d06c89de70f3b2d0ed06f90a24
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2025-12-30 11:59:21 +0100

    Added allure reporting to CI

---
+++ tests/e2e/allure-check.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('ALLURE CHECK - forced fail', async ({ page }) => {
+  await page.goto('/');
+  expect(true).toBe(false); // This assertion is intended to fail
+});
