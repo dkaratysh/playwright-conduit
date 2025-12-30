4f85d2e40919324d8b8e21b14e2e48c2aa081ecf
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2025-12-30 12:36:24 +0100

    test commit

--- tests/e2e/allure-check.spec.ts
+++ tests/e2e/allure-check.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('ALLURE CHECK - forced fail', async ({ page }) => {
   await page.goto('/');
-  expect(true).toBe(false); // This assertion is intended to fail
+  expect(true).toBe(false); // This assertion is intended to fail for Allure report demonstration
 });
