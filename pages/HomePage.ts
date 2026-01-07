1dfef38c9bb514f937e25ed91947b7fb17183d58
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-07 18:02:28 +0100

    Separate navbar component from home page

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,20 +1,30 @@
 import { BasePage } from './BasePage';
 import type { Locator, Page } from '@playwright/test';
+import { NavbarComponent } from '../components/navbar.component';
 
 export class HomePage extends BasePage {
-  readonly newArticleButton: Locator;
+  
+  readonly navbar: NavbarComponent;
+
+  readonly banner: Locator;
+  readonly globalFeedTab: Locator;
+  readonly yourFeedTab: Locator;
+  
 
   constructor(page: Page) {
     super(page);
+    this.navbar = new NavbarComponent(this.page);
 
-    this.newArticleButton = page.getByRole('link', { name: 'New Article' });
-  }
-
-  async open() {
-    await this.page.goto('/');
+    this.banner = this.page.locator('.banner');
+    this.globalFeedTab = this.page.getByRole('link', { name: 'Global Feed' });
+    this.yourFeedTab = this.page.getByRole('link', { name: 'Your Feed' });
   }
 
   async goToEditor() {
-    await this.newArticleButton.click();
+    await this.navbar.goToEditor();
+  }
+
+  async goto() {
+    await this.page.goto('/');
   }
 }
