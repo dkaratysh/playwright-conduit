0498e75518fbd71c98890cbd9a59eb97a162978a
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-14 16:29:26 +0100

    Added tags smoke test

---
+++ tests/api/tags.smoke.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test ('[API][SMOKE][TAGS] GET /api.tags - should return list of public tags', async ({ request }) => {
+
+    const response = await request.get('/api/tags');
+
+    expect (response.status()).toBe(200);
+
+    const body = await response.json();
+    expect (Array.isArray(body.tags)).toBe(true);
+});
