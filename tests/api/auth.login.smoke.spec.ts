f7111dab4b2a5e3d39680dbf879914a9d5b6bac3
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-14 14:51:11 +0100

    Create auth.login.smoke.spec.ts

---
+++ tests/api/auth.login.smoke.spec.ts
@@ -0,0 +1,24 @@
+import {test, expect} from '@playwright/test';
+import {user} from '../../test-data/auth/user.data';
+
+test ('[API] [SMOKE] [AUTH] POST /api/users/login - should return token', async ({ request }) => {
+    const response = await request.post('/api/users/login', {
+        data:{
+            user: {
+                email: user.email,
+                password: user.password
+            },
+        },
+    });
+
+    expect (response.ok()).toBeTruthy();
+    expect (response.status()).toBe(200);
+
+    const body = await response.json();
+    expect (body).toHaveProperty('user');
+    expect (body.user).toHaveProperty('token');
+
+    expect (typeof body.user.token).toBe('string');
+    expect (body.user.token.length).toBeGreaterThan(10);
+});
+
