095e56b3b60339a2d25eea73935474737c90c168
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-16 12:25:01 +0100

    Refactored signup api smoke test

--- tests/api/smoke/signup.smoke.spec.ts
+++ tests/api/smoke/signup.smoke.spec.ts
@@ -1,17 +1,15 @@
 import {test, expect} from '@playwright/test';
+import { createTempUser } from '../../../helpers/api/temp.user.helper'
 
 test ('POST api/users registers a new user and login successfully', async ({ request }) => {
-    const username = `smoke_${Date.now()}`
-    const email = `smoke_${Date.now()}@test.local`
-    const password = 'Test123'
 
-    await request.post('api/users', {
-        data: { user: { email, password, username}}
+    const tempUser = await createTempUser(request);
+    const loginResponse = await request.post('api/users/login', {
+        data: { user: {email: tempUser.email, password: tempUser.password}}
     })
 
-    const login = await request.post('api/users/login', {
-        data: { user: { email, password}}
-    })
-
-    expect(login.ok()).toBeTruthy();
+    expect(loginResponse.ok()).toBeTruthy();
+    const body = await loginResponse.json();
+    expect(body).toHaveProperty('user.token');
+    expect(typeof body.user.token).toBe('string');
 });
