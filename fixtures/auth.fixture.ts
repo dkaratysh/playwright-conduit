3db392432c88bd753cd1df59368febb18e863c79
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-26 10:56:51 +0100

    Auth fixture returns testUser

--- fixtures/auth.fixture.ts
+++ fixtures/auth.fixture.ts
@@ -1,18 +1,30 @@
 import { test as base, expect } from '@playwright/test';
 import type { AuthToken } from '../types/auth';
 import { loginViaApi } from '../helpers/api/auth';
-import { user } from '../test-data/auth/user.data';
+import { user as testUser } from '../test-data/auth/user.data';
 
 export const test = base.extend<{
   authToken: AuthToken;
   authTokenB: AuthToken;
+  authUser: typeof testUser
+  authUserB: typeof testUser
+
 }>({
+  authUser: async({}, use ) => {
+    await use(testUser)
+  },
+  
   authToken: async ({ request }, use) => {
-    const token = await loginViaApi(request, user);
+    const token = await loginViaApi(request, testUser);
     await use(token);
   },
+
+  authUserB: async({}, use ) => {
+    await use(testUser)
+  },
+
   authTokenB: async ({ request }, use) => {
-    const token = await loginViaApi(request, user);
+    const token = await loginViaApi(request, testUser);
     await use(token);
   },
 });
--- tests/api/critical/auth/auth-contract.spec.ts
+++ tests/api/critical/auth/auth-contract.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '../../../../fixtures/auth.fixture';
-import { user as testUser } from '../../../../test-data/auth/user.data';
 
-test('login token works on protected endpoint', async ({ request, authToken }) => {
+
+test('login token works on protected endpoint', async ({ request, authToken, authUser }) => {
   expect(authToken).toBeTruthy();
 
   const response = await request.get('/api/user', {
@@ -11,5 +11,5 @@ test('login token works on protected endpoint', async ({ request, authToken }) =
   expect(response.status()).toBe(200);
 
   const { user } = await response.json();
-  expect(user.email).toBe(testUser.email);
+  expect(user.email).toBe(authUser.email);
 });
