e25338bb10b2300a4b7e2bd463fbf75ecef347da
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-28 17:56:08 +0100

    Added edit article test

---
+++ tests/ui/critical/edit-article.spec.ts
@@ -0,0 +1,18 @@
+import { test } from '../../../fixtures/article.fixture';
+import { makeUpdatedArticle } from '../../../helpers/ui/article.helpers';
+import { Pages } from '../../../pages/pages.factory';
+
+  test('Update the article', async ({ article, page }) => {
+    const updatedArticle = makeUpdatedArticle(article);
+    let pages: Pages;
+    pages = new Pages(page);
+
+    await pages.article.openArticle(article.slug);
+    await pages.article.assertOpened(article.slug);
+    await pages.article.updateArticle(updatedArticle);
+
+    await pages.article.expectTitle(updatedArticle.title);
+    await pages.article.expectBody(updatedArticle.body);
+  });
+
+  
