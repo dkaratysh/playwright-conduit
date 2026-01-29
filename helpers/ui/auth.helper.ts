1d22759318d0b0aad83fb22a8ab0b1fa80bbc22d
dkaratysh <87898207+dkaratysh@users.noreply.github.com>
2026-01-29 15:13:51 +0100

    Page objects refactoring

--- helpers/ui/auth.helper.ts
+++
@@ -1,7 +0,0 @@
-import type { Page } from '@playwright/test';
-
-export async function loginAs(page: Page, token: string) {
-  await page.addInitScript(token => {
-    localStorage.setItem('jwt', token);
-  }, token);
-}
